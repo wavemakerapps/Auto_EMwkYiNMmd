633f2db6010506ba320cf395ef015feea5522f2a
root <root@61949b3d2f95>
2017-07-05 13:43:13 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_EMwkYiNMmd",
+  "homePage" : "Main",
+  "name" : "Auto_EMwkYiNMmd",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
